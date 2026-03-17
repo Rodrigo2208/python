e525813ad2286c542fed110fad830eb59fd5c871
Rodrigo Durães <rodrigoduraes.nascimento11@gmail.com>
2026-03-17 08:01:49 -0300

    Update app.py

--- ex020/app.py
+++ ex020/app.py
@@ -1,11 +1,3 @@
-x = int(input("Digite o valor de X: "))
-n = int(input("Digite o valor de N: "))
+x = int(input("Coloque um número qualquer: "))
 
-soma = 0
-num = 1
-
-while num <= n:
-    soma = soma + x ** num
-    num = num + 1
-
-print("Resultado:", soma)
+print(f"{x} + {x}^2 + {x}^4 = {x + x ** 2 + x **4}")
