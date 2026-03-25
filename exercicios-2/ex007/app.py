e141118025d479388b5e4af4779b399ad5552b36
Rodrigo Durães <rodrigoduraes.nascimento11@gmail.com>
2026-03-25 16:45:47 -0300

    Create app.py

---
+++ exercicios-2/ex007/app.py
@@ -0,0 +1,12 @@
+n = int(input("Coloque números inteiros: "))
+f = 0
+q = n
+total = 1
+
+while f < q:
+    total = total * n
+    f = f + 1
+    n = n - 1
+
+print(f"O fatorial de {q}! é {total}.")
+
