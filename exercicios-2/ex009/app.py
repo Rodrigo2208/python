dfc5fa14614ad3d6081b92b7d2c44ef1cc33b1ef
Rodrigo Durães <rodrigoduraes.nascimento11@gmail.com>
2026-03-28 10:29:21 -0300

    Update app.py

--- exercicios-2/ex009/app.py
+++ exercicios-2/ex009/app.py
@@ -1,7 +1,28 @@
-n = int(input("Coloque números inteiros: "))
-i = 1
+a1 = int(input("insira um valor: "))
+a2 = int(input("insira um valor: "))
+a3 = int(input("insira um valor: "))
+a4 = int(input("insira um valor: "))
+a5 = int(input("insira um valor: "))
 
-while i < 5:
-    n = int(input("Coloque números inteiros: "))
-    i = i + 1
-if 
+maior = a1
+menor = a1
+
+if a2 > a1:
+    maior = a2
+elif a3 > a2:
+    maior = a3
+elif a4 > a3:
+    maior = a4
+elif a5 > a4:
+    maior = a5
+
+if a2 < a1:
+    menor = a2
+elif a3 < a2:
+    menor = a3
+elif a4 < a3:
+    menor = a4
+elif a5 < a4:
+    menor = a5
+    
+print(f"O maior número é {maior} e o menor é {menor}.")
