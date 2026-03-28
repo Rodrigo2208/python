8259b91ce4200fcaf2997595cf0b2982ed262124
Rodrigo Durães <rodrigoduraes.nascimento11@gmail.com>
2026-03-28 10:29:26 -0300

    Delete app.py

--- testes-do-beecrowd/app.py
+++
@@ -1,8 +0,0 @@
-cod_peca1, num_peca1, val_peca1 = input().split()
-cod_peca1 = int(cod_peca1)
-num_peca1 = int(num_peca1)
-val_peca1 = float(val_peca1)
-
-tot_peca1 = num_peca1 * val_peca1
-
-print(f"VALOR A PAGAR: R$ {tot_peca1:.2f}")
