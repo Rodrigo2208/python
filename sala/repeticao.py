5fd63e0dd99f52e073a8a8f0a8b80a898b761a61
Rodrigo Durães <rodrigoduraes.nascimento11@gmail.com>
2026-04-24 07:30:45 -0300

    Create repeticao.py

---
+++ sala/repeticao.py
@@ -0,0 +1,16 @@
+for i in range(1,10):
+    print (i*2)
+for n in [24, 67, 69, 87, 21, 2.4]:
+    print (f"n:{n-0.00000000000002}")
+print("fim")
+nome = "coisa"
+for letra in nome:
+    print (f"letra: {letra}")
+vale =  1
+soma = 0
+while vale != 0:
+    vale = int(input("Digite um valor: "))
+    soma += vale
+print(f"soma = {soma}")
+
+    
